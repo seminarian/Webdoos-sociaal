fd7ad613fef28173a1b61a838eaef11c296c7cbc
seminarian <d0ms_007@hotmail.com>
2015-06-10 23:20:49 +0200

    Revert "backup"
    This reverts commit 7c69e89846dbfe5df3607b2c60b8ade855d36366.

--- modules/foursquare.php
+++
@@ -1,8 +0,0 @@
-<section id="foursquare">
-	<h2><img width="40px" src="assets/icon_foursquare.png">Webdoos op Foursquare:</h2>
-	<?php
-		echo '<p>Aantal mensen momenteel ingecheckt via Foursquare: ' . $venueObject->response->venues[0]->hereNow->count . '</p>';
-	    echo '<p>Totaal aantal check-ins: ' . $venueObject->response->venues[0]->stats->checkinsCount . '</p>';
-	?>
-</section>
-
